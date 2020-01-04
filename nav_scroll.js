fc9540a338fc27e14bb031687fb37ef555ca8eb5
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-01-04 20:23:54 +0100

    MJ #30

--- nav_scroll.js
+++ nav_scroll.js
@@ -3,32 +3,32 @@ window.onscroll = function() {functionScroll()};
 var vHome = 0;
 var vProjects = 1045;
 var vAboutMe = 2161;
-var vContacts = 4000;
+var vContacts = 2900;
 
 
 function functionScroll() {
     if (document.documentElement.scrollTop < vProjects) {
-        document.getElementById("nav_home").className = "";
-        document.getElementById("nav_projects").className = "opacity";
-        document.getElementById("nav_about_me").className = "opacity";
-        document.getElementById("nav_contact").className = "opacity";
+        document.getElementById("nav_home").className = "nav_on";
+        document.getElementById("nav_projects").className = "nav_off";
+        document.getElementById("nav_about_me").className = "nav_off";
+        document.getElementById("nav_contact").className = "nav_off";
     }
     if (document.documentElement.scrollTop > vProjects && document.documentElement.scrollTop < vAboutMe) {
-        document.getElementById("nav_home").className = "opacity";
-        document.getElementById("nav_projects").className = "";
-        document.getElementById("nav_about_me").className = "opacity";
-        document.getElementById("nav_contact").className = "opacity";
+        document.getElementById("nav_home").className = "nav_off";
+        document.getElementById("nav_projects").className = "nav_on";
+        document.getElementById("nav_about_me").className = "nav_off";
+        document.getElementById("nav_contact").className = "nav_off";
     }
     if (document.documentElement.scrollTop > vAboutMe && document.documentElement.scrollTop < vContacts) {
-        document.getElementById("nav_home").className = "opacity";
-        document.getElementById("nav_projects").className = "opacity";
-        document.getElementById("nav_about_me").className = "";
-        document.getElementById("nav_contact").className = "opacity";
+        document.getElementById("nav_home").className = "nav_off";
+        document.getElementById("nav_projects").className = "nav_off";
+        document.getElementById("nav_about_me").className = "nav_on";
+        document.getElementById("nav_contact").className = "nav_off";
     }
     if (document.documentElement.scrollTop > vContacts) {
-        document.getElementById("nav_home").className = "opacity";
-        document.getElementById("nav_projects").className = "opacity";
-        document.getElementById("nav_about_me").className = "opacity";
-        document.getElementById("nav_contact").className = "";
+        document.getElementById("nav_home").className = "nav_off";
+        document.getElementById("nav_projects").className = "nav_off";
+        document.getElementById("nav_about_me").className = "nav_off";
+        document.getElementById("nav_contact").className = "nav_on";
     }
 }
