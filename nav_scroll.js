dc9f1d70676a13fb0ce501762d41c6688c6afe8c
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-01-11 19:11:46 +0100

    MJ #60

--- nav_scroll.js
+++ nav_scroll.js
@@ -1,10 +1,28 @@
 window.onscroll = function() {functionScroll()};
   
-var vHome = 0;
-var vProjects = 1045;
-var vAboutMe = 2161;
-var vContacts = 3735;
 
+
+function vh(v) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+}
+  
+function vw(v) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (v * w) / 100;
+}
+  
+function vmin(v) {
+    return Math.min(vh(v), vw(v));
+}
+  
+function vmax(v) {
+    return Math.max(vh(v), vw(v));
+}
+
+var vProjects = vh(95);
+var vAboutMe = vh(195);
+var vContacts = vh(395);
 
 function functionScroll() {
     if (document.documentElement.scrollTop < vProjects) {
