6f4b98bc948ad15662bc8930383299c4bff87a69
Mrozuu <58520713+Mrozuu@users.noreply.github.com>
2020-03-09 19:54:56 +0100

    MJ #153

--- nav_scroll.js
+++ nav_scroll.js
@@ -29,7 +29,8 @@ var vProjects = vh(95);
 var vAboutMe = vh(195);
 var vContacts = vAboutMe + vh(5) + document.getElementById("about_me").offsetHeight;
 console.log(vContacts);
-
+var vContacts3 = document.getElementById("fullsite").offsetHeight - document.getElementById("content").offsetHeight;
+console.log(vContacts3) + console.log('!!!');
 
 var vContacts2;
 $(document).ready(function () {
